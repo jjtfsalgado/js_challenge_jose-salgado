4f93211b5d0ab298427d13d5ee5926b478a0a486
José Salgado <jjtfsalgado@gmail.com>
2017-06-24 14:20:27 +0100

    Update sagas.js

--- src/sagas/sagas.js
+++ src/sagas/sagas.js
@@ -1,22 +1,22 @@
-import { takeEvery, fork, put, call } from 'redux-saga/effects';
-import { delay } from 'redux-saga';
+// import { takeEvery, fork, put, call } from 'redux-saga/effects';
+// import { delay } from 'redux-saga';
 
-function* changePage(action) {
-  console.log(action);
-  try {
-    const pagination = yield put({ type: 'MODIFY_PAGINATION', payload: action.payload });
-    console.log(pagination);
-  } catch(error) {
-    yield put({ type: 'MODIFY_PAGINATION', payload: error });
-  }
-}
+// function* changePage(action) {
+//   console.log(action);
+//   try {
+//     const pagination = yield put({ type: 'MODIFY_PAGINATION', payload: action.payload });
+//     console.log(pagination);
+//   } catch(error) {
+//     yield put({ type: 'MODIFY_PAGINATION', payload: error });
+//   }
+// }
 
-function* watchHelloAsync() {
-  yield takeEvery("sf", changePage);
-}
+// function* watchHelloAsync() {
+//   yield takeEvery("sf", changePage);
+// }
 
-export default function* rootSaga() {
-  yield [
-    fork(watchHelloAsync)
-  ];
-}
+// export default function* rootSaga() {
+//   yield [
+//     fork(watchHelloAsync)
+//   ];
+// }
