f5c04b4cbc9231fd4de0d788e70c216f7af637c3
José Salgado <jjtfsalgado@gmail.com>
2017-06-25 20:43:29 +0100

    Removed redux-saga

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import "babel-polyfill";
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import createSagaMiddleware from 'redux-saga';
 import { createLogger } from 'redux-logger';
 import { createStore, applyMiddleware, combineReducers} from 'redux';
 import { Router, hashHistory } from 'react-router';
@@ -10,10 +9,7 @@ import { syncHistoryWithStore, routerReducer, routerMiddleware } from 'react-rou
 
 import './styles/app.scss';
 import { productsReducer } from './reducers/reducer';
-import rootSaga from './sagas/sagas';
 import router from './router';
-
-const sagaMiddleware = createSagaMiddleware();
 
 const reducers = combineReducers({
   products: productsReducer,
@@ -22,12 +18,10 @@ const reducers = combineReducers({
 
 const store = createStore(
   reducers,
-  applyMiddleware(sagaMiddleware, createLogger(), routerMiddleware(hashHistory))
+  applyMiddleware(createLogger(), routerMiddleware(hashHistory))
 )
 
 const history = syncHistoryWithStore(hashHistory, store)
-
-sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(
   <Provider store={ store }>
